45a1d52af4309fb082904e61fa5bf22a011c8638
michellehsieh <michelle.hsieh035@gmail.com>
2017-05-07 14:12:56 +0800

    練習更新 EDMX 實體資料模型 ( 從資料庫更新模型 ) varchar -> nvarchar

--- MVC5Course/Models/Product.cs
+++ MVC5Course/Models/Product.cs
@@ -11,8 +11,7 @@ namespace MVC5Course.Models
 {
     using System;
     using System.Collections.Generic;
-    using System.ComponentModel.DataAnnotations;
-
+    
     public partial class Product
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -26,8 +25,7 @@ namespace MVC5Course.Models
         public Nullable<decimal> Price { get; set; }
         public Nullable<bool> Active { get; set; }
         public Nullable<decimal> Stock { get; set; }
-        
-
+    
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<OrderLine> OrderLine { get; set; }
     }
