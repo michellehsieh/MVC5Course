9877d4d5c1c2112b8cb7f554eff145783ee393dc
michellehsieh <michelle.hsieh035@gmail.com>
2017-05-07 16:32:46 +0800

    練習自訂輸入驗證的驗證屬性

--- MVC5Course/Models/Product.Partial.cs
+++ MVC5Course/Models/Product.Partial.cs
@@ -5,6 +5,7 @@ namespace MVC5Course.Models
     using System.ComponentModel;
     using System.ComponentModel.DataAnnotations;
     using System.Linq;
+    using ValidationAttributes;
 
     [MetadataType(typeof(ProductMetaData))]
     public partial class Product
@@ -36,6 +37,7 @@ namespace MVC5Course.Models
         //[RegularExpression("(.+)-(.+)", ErrorMessage = "商品名稱格式錯誤！")]
         [StringLength(80, ErrorMessage="欄位長度不得大於 80 個字元")]
         [DisplayName("商品名稱")]
+        [商品名稱必須包含will字串(ErrorMessage = "商品名稱必須包含Will字串")]
         public string ProductName { get; set; }
 
         [Required(ErrorMessage = "請輸入商品價格！")]
---
+++ MVC5Course/Models/ValidationAttributes/商品名稱必須包含will字串Attribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVC5Course.Models.ValidationAttributes
+{
+    public class 商品名稱必須包含will字串Attribute : DataTypeAttribute
+    {
+        public 商品名稱必須包含will字串Attribute() : base(DataType.Text)
+        {
+
+        }
+
+        public override bool IsValid(object value) {
+            var str = (string)value;
+            return str.Contains("Will");
+        }
+    }
+}
