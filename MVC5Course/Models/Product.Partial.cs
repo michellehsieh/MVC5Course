228199cad12ca5c3bf662f1dab7066a2fa9d9b5b
michellehsieh <michelle.hsieh035@gmail.com>
2017-05-07 17:01:28 +0800

    練習自訂模型驗證

--- MVC5Course/Models/Product.Partial.cs
+++ MVC5Course/Models/Product.Partial.cs
@@ -8,7 +8,7 @@ namespace MVC5Course.Models
     using ValidationAttributes;
 
     [MetadataType(typeof(ProductMetaData))]
-    public partial class Product
+    public partial class Product : IValidatableObject
     {
         public int 訂單數量 {
             get {
@@ -25,6 +25,25 @@ namespace MVC5Course.Models
                 return this.OrderLine.Count(p => p.Qty > 400);
             }
         }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (this.Price > 100 && this.Stock > 5) {
+                var db = new FabricsEntities();
+                yield return new ValidationResult("價格與庫存數量不合理", new string[] { "Price", "Stock"});
+
+            }
+
+            //條件：this.OrderLine.Count() == 1 && this.Stock == 0
+            if (this.Stock == 0)
+            {
+                yield return new ValidationResult("Stock與訂單數量不匹配", new string[] { "Stock" });
+            }
+            
+            //結束
+            yield break;
+            //throw new NotImplementedException();
+        }
     }
     
     public partial class ProductMetaData
@@ -35,7 +54,7 @@ namespace MVC5Course.Models
         [Required(ErrorMessage = "請輸入商品名稱！")]
         //[MinLength(3), MaxLength(30)]
         //[RegularExpression("(.+)-(.+)", ErrorMessage = "商品名稱格式錯誤！")]
-        [StringLength(80, ErrorMessage="欄位長度不得大於 80 個字元")]
+        //[StringLength(80, ErrorMessage="欄位長度不得大於 80 個字元")]
         [DisplayName("商品名稱")]
         [商品名稱必須包含will字串(ErrorMessage = "商品名稱必須包含Will字串")]
         public string ProductName { get; set; }
